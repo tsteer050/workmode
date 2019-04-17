58d5e7fe9b23a675f9af0313930c0f7430d6a10e
tsteer <trevor.piano@gmail.com>
2019-04-17 15:08:41 -0700

    Fixing for heroku

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-// import SignupFormContainer from './session/signup_form_container';
-// import LoginFormContainer from './session/login_form_container';
-import { Route, Switch } from 'react-router-dom';
-// import SplashContainer from './splash/splash_container';
 import SplashWindow from './splash/splash_window/splash_window';
 import { AuthRoute, ProtectedRoute } from '../util/route_utils';
 import MainView from './main/main_view';
