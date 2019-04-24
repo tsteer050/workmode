96e5cc5a34bc678a44dc14664dd66ca1068eed15
tsteer <trevor.piano@gmail.com>
2019-04-23 17:26:49 -0700

    Got ActionCableProvider working

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
+import ActionCableProvider from 'react-actioncable-provider';
 
-const Root = ({ store }) => (
+const Root = ({ store }) => {
+  debugger
+  return(
   
   <Provider store={store}>
     <HashRouter>
+      <ActionCableProvider url='http://localhost:3000'>
       <App />
+      </ActionCableProvider>
     </HashRouter>
   </Provider>
-)
+)}
 
 export default Root;
 
