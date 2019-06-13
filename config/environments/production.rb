38627c71dd5d3ed1851a761675a9ce2063c71ac1
tsteer <trevor.piano@gmail.com>
2019-06-12 17:13:11 -0700

    Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+  config.web_socket_server_url = "wss://workmode.herokuapp.com/cable" 
+config.action_cable.allowed_request_origins = ['https://workmode.herokuapp.com', 'http://workmode.herokuapp.com']
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
